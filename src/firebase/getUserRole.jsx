74b433995eb4719616f6e390f2129322385c1e43
Azfar-git <azfarali.edu@gmail.com>
2026-04-05 22:01:27 +0500

    dd..

--- src/firebase/getUserRole.jsx
+++
@@ -1,8 +0,0 @@
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "./config";
-
-export const getUserRole = async (uid) => {
-  const docRef = doc(db, "users", uid);
-  const snap = await getDoc(docRef);
-  return snap.data()?.role;
-};
